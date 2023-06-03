850bc0d55b27da2492155167d2ebfb3f0e00d63b
bokhtiar <bokhtiar@gmail.com>
2023-06-03 23:47:08 +0600

    Village edit update && destory done

--- app/Http/Controllers/VillageController.php
+++ app/Http/Controllers/VillageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\VillageRequest;
 use App\Models\Village;
 use App\Services\CityService;
 use App\Services\VillageService;
@@ -26,7 +27,7 @@ class VillageController extends Controller
     {}
 
     /* Store a newly created resource in storage. */
-    public function store(Request $request)
+    public function store(VillageRequest $request)
     {
         try {
             VillageService::storeResource($request);
@@ -40,27 +41,38 @@ class VillageController extends Controller
     public function show(Village $village)
     {}
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Village $village)
+    /* Show the form for editing the specified resource. */
+    public function edit($id)
     {
-        //
+        try {
+            $cities = CityService::findAll();
+            $villages = VillageService::findAll();
+            $edit = VillageService::findById($id);
+            return view('modules.village.index', compact('villages', 'cities', 'edit'));
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Village $village)
+    /* Update the specified resource in storage. */
+    public function update(VillageRequest $request, $id)
     {
-        //
+        try {
+            VillageService::findByIdAndUpdate($id, $request);
+            return redirect()->route('village.index');
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Village $village)
+    /* Remove the specified resource from storage. */
+    public function destroy($id)
     {
-        //
+        try {
+            VillageService::findById($id)->delete();
+            return redirect()->route('village.index');
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 }
